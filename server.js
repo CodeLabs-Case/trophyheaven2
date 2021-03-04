6fa3ffb8ec59fee29ac59b9de5029245e4f1fe65
blackBoxMod <davis.gamer07@gmail.com>
2021-03-04 04:58:26 -0500

    Integrating stripe 4

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 // These will be set inside the hosting environment in Amazon Web Services in the production application
 require('dotenv')
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY
+const stripePublicKey = process.env.STRIPE_PUBLIC_KEY
 
 const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
 
 const app = express()
 
@@ -18,4 +22,34 @@ app.listen(port, (err) => {
         console.log(err)
     }
     console.log("Server is live")
+})
+
+app.get('/', (req, res) => {
+
+    res.render('/var/app/current/views/index.ejs', {key, stripePublicKey})
+})
+
+app.post('/payment', (req, res) => {
+    stripe.customers.create({
+        email: req.body.stripeEmail,
+        source: req.body.stripeToken,
+        name: "",
+        address: {
+            line1: "",
+            postal_code: "",
+            state: "",
+            country: ""
+        }
+    })
+    .then(cusomter => {
+        return stripe.charges.create({
+            amount: 1000,
+            description: "",
+            currency: 'USD',
+            customer: cusomter.id
+        })
+    })
+    .then(charge => {
+        res.send('Success')
+    })
 })
