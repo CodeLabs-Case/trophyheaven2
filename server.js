9b525dba76d373b6d6b2b1fc1af842220b98362b
blackBoxMod <davis.gamer07@gmail.com>
2021-03-05 02:15:20 -0500

    Integrating stripe 83

--- server.js
+++ server.js
@@ -51,6 +51,7 @@ app.post('/payment', (req, res) => {
 
     var subtotal = req.body.subtotal
     var subtotal_int = parseInt(subtotal, 10)
+    console.log(subtotal_int)
     // Create and send the payment
     stripe.customers.create({
         email: "davis.architect99@gmail.com",//req.body.stripeEmail
