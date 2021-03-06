226a4e55497368c4e8e06bcc2c515042f98c255f
blackBoxMod <davis.gamer07@gmail.com>
2021-03-06 04:11:01 -0500

    COVID alert. 14

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -270,7 +270,7 @@ class UI {
             button.addEventListener('click', event => {
                 const amountBeforeAdd = Storage.getLength()
                 event.target.innerText = "In Cart"
-                event.target.disabled = true
+                event.target.removeEventListener('click')
     
                 // get product from local storage and use the spread operator to add a quatity field
                 let cartItem = {...Storage.getProduct(id), amount: 1}
