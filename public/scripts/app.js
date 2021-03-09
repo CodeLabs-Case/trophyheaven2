99a54049d03c10413bcb12d31331228e73348bb4
blackBoxMod <davis.gamer07@gmail.com>
2021-03-09 08:56:21 -0500

    Paypal integration

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -619,3 +619,33 @@ document.addEventListener("DOMContentLoaded", () => {
         navState = false
     })
 })
+
+
+// Paypal
+function initPayPalButton() {
+    paypal.Buttons({
+        style: {
+            shape: 'rect',
+            color: 'blue',
+            layout: 'vertical',
+            label: 'checkout',
+        },
+
+        createOrder: function(data, actions) {
+            return actions.order.create({
+            purchase_units: [{"amount":{"currency_code":"USD","value":0}}]
+            });
+        },
+
+        onApprove: function(data, actions) {
+            return actions.order.capture().then(function(details) {
+            alert('Transaction completed by ' + details.payer.name.given_name + '!');
+            });
+        },
+
+        onError: function(err) {
+            console.log(err);
+        }
+    }).render('#paypal-button-container');
+}
+initPayPalButton();
