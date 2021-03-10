ebc0796bf276f5ffda3b5c346772154cc41e9d8a
blackBoxMod <davis.gamer07@gmail.com>
2021-03-10 12:12:10 -0500

    Paypal test 26

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -619,33 +619,3 @@ document.addEventListener("DOMContentLoaded", () => {
         navState = false
     })
 })
-
-// Paypal
-function initPayPalButton() {
-    paypal.Buttons({
-      style: {
-        shape: 'rect',
-        color: 'blue',
-        layout: 'vertical',
-        label: 'checkout',
-        
-      },
-
-      createOrder: function(data, actions) {
-        return actions.order.create({
-          purchase_units: [{"amount":{"currency_code":"USD","value":parseFloat(totalDOM.innerText).toFixed(2)}}]
-        });
-      },
-
-      onApprove: function(data, actions) {
-        return actions.order.capture().then(function(details) {
-          alert('Transaction completed by ' + details.payer.name.given_name + '!');
-        });
-      },
-
-      onError: function(err) {
-        console.log(err);
-      }
-    }).render('#paypal-button-container');
-}
-initPayPalButton();
